585a6b1521a927c039039f583ec1c4ff5e8042b9
yanggang.james <yanggang.james@bytedance.com>
2020-11-24 11:43:51 +0800

    feat: add upload sts2 logic and demo.

--- volcengine/vod/VodUploadService.py
+++ volcengine/vod/VodUploadService.py
@@ -130,4 +130,14 @@ class VodUploadService(VodService):
         else:
             return Parse(res, VodCommitUploadInfoResponse(), True)
 
-# end of service interface
+    # end of service interface
+
+    def get_upload_sts2_with_expired_time(self, expired_time):
+        actions = ["vod:ApplyUploadInfo", "vod:CommitUploadInfo"]
+        resources = []
+        statement = Statement.new_allow_statement(actions, resources)
+        inline_policy = Policy([statement])
+        return self.sign_sts2(inline_policy, expired_time)
+
+    def get_upload_sts2(self):
+        return self.get_upload_sts2_with_expired_time(60 * 60)
