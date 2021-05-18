1fd8858248e26cd3faf37d57d9a0080716a0d29c
王一帆 <wangyifan.co@bytedance.com>
2021-05-18 16:03:36 +0800

    game product

---
+++ volcengine/example/game_protect/example_anti_plugin.py
@@ -0,0 +1,21 @@
+
+from volcengine.game_protect.GameProtectService import GameProtectService
+
+if __name__ == '__main__':
+    gameProtector = GameProtectService()
+
+    # call below method if you dont set ak and sk in $HOME/.volc/config
+    gameProtector.set_ak('***REMOVED***')
+    gameProtector.set_sk('***REMOVED***')
+
+    params = {
+        'AppId': 218745,
+        'StartTime': 1618502400,
+        'EndTime': 1618545491,
+        'PageSize': 10,
+        'PageNum': 1
+    }
+    req = dict()
+
+    resp = gameProtector.risk_result(params, req)
+    print resp
