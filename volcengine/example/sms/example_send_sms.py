9d9a92c94c181a3101733aafcbf80005894e1eca
wangyang <wangyang.a9@bytedance.com>
2021-08-05 13:54:06 +0800

    [Feature]volc SMS SDK add sg region

--- volcengine/example/sms/example_send_sms.py
+++ volcengine/example/sms/example_send_sms.py
@@ -4,9 +4,12 @@ from __future__ import print_function
 import json
 
 from volcengine.sms.SmsService import SmsService
+from volcengine.const.Const import *
 
 if __name__ == '__main__':
     sms_service = SmsService()
+    # oversea
+    # sms_service = SmsService(REGION_AP_SINGAPORE1)
 
     # call below method if you dont set ak and sk in $HOME/.volc/config
     sms_service.set_ak('ak')
