9da4bd882d84842dafb0cb69fb40beb90f1ee9c0
leiyixing <leiyixing@bytedance.com>
2021-12-30 20:56:39 +0800

    fix: fix utf-8 encode for python2

--- volcengine/util/Util.py
+++ volcengine/util/Util.py
@@ -41,7 +41,7 @@ class Util(object):
         if sys.version_info[0] == 3:
             return hmac.new(key, bytes(content, encoding='utf-8'), hashlib.sha256).digest()
         else:
-            return hmac.new(key, content, hashlib.sha256).digest()
+            return hmac.new(key, content.encode('utf-8'), hashlib.sha256).digest()
 
     @staticmethod
     def hmac_sha1(key, content):
@@ -49,7 +49,7 @@ class Util(object):
         if sys.version_info[0] == 3:
             return hmac.new(key, bytes(content, encoding='utf-8'), hashlib.sha1).digest()
         else:
-            return hmac.new(key, content, hashlib.sha1).digest()
+            return hmac.new(key, content.encode('utf-8'), hashlib.sha1).digest()
 
     @staticmethod
     def sha256(content):
@@ -100,7 +100,7 @@ class Util(object):
             crypt = generator.encrypt(Util.pad(orig_data).encode('utf-8'))
             return base64.b64encode(crypt).decode()
         else:
-            crypt = generator.encrypt(Util.pad(orig_data))
+            crypt = generator.encrypt(Util.pad(orig_data)).encode('utf-8')
             return base64.b64encode(crypt)
 
     @staticmethod
