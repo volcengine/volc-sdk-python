cfa38aebb3f3e82cf6522abd65a53143cb823c93
lvwenhua <lvwenhua@bytedance.com>
2022-03-07 16:10:28 +0800

    feat: live add api

---
+++ volcengine/example/live/example_domain/example_disable_domain.py
@@ -0,0 +1,17 @@
+# coding:utf-8
+import json
+
+from volcengine.live.LiveService import LiveService
+
+if __name__ == '__main__':
+    live_service = LiveService()
+    ak = ""
+    sk = ""
+    live_service.set_ak(ak)
+    live_service.set_sk(sk)
+    body = {
+
+    }
+    body = json.dumps(body)
+    resp = live_service.disable_domain(body)
+    print(resp)
