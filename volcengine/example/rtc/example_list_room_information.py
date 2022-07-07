a3b9f7891ea071685dcc315f41a4d9d1d1fcba1c
tiandahai <tiandahai@bytedance.com>
2022-07-07 14:22:43 +0800

    feat: update interface

--- volcengine/example/rtc/example_list_room_information.py
+++ volcengine/example/rtc/example_list_room_information.py
@@ -18,8 +18,12 @@ if __name__ == '__main__':
     # You can using this API now ! Here are some Examples
     params = dict()
     params['AppId'] = 'Your_AppId'
+    params['StartTime'] = '2022-07-06T12:00:00+08:00'
+    params['EndTime'] = '2022-07-06T14:00:00+08:00'
     # optional params
     # params['RoomId'] = 'Your_RoomId'
+    # params['PageNum'] = 'PageNum'
+    # params['PageSize'] = 'PageSize'
 
-    resp = rtc_service.list_rooms(params)
+    resp = rtc_service.list_room_information(params)
     print(resp)
