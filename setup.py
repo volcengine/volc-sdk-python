53d2a7bb1bcd4fca77ac986bb8983b9baaeb8674
郭志峰 <guozhifeng.felton@bytedance.com>
2023-07-27 13:06:08 +0000

    fix(dependencies): upgrade dependencies to fix vulnerabilities
    * fix(dependencies): upgrade dependencies to fix vulnerabilities
    * fix(dependencies): upgrade dependencies to fix vulnerabilities
    * Merge remote-tracking branch 'origin/fix_dependency' into fix_dependency
    
    https://code.byted.org/volcengine/volc-sdk-python/merge_requests/438

--- setup.py
+++ setup.py
@@ -4,7 +4,15 @@ import sys
 from setuptools import setup, find_packages
 from volcengine import VERSION
 
-install_requires = ["requests", "retry", "pytz", "pycryptodome", "protobuf", "google", "six"]
+install_requires = [
+    "requests>=2.25.1",
+    "retry==0.9.2",
+    "pytz==2020.5",
+    "pycryptodome==3.9.9",
+    "protobuf~=3.19.0",
+    "google>=3.0.0",
+    "six>=1.0"
+]
 
 setup(
     name="volcengine",
