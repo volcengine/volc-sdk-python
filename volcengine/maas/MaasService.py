902933e1a5a4d90eceac304ad685b70581e07d0f
刘慧琦 <liuhuiqi.7@bytedance.com>
2023-08-24 12:03:45 +0000

    feat(maas) 增加密文输入输出的能力
    * ka manager py
    * cert and encrypt and decrypt
    * requirements
    
    https://code.byted.org/volcengine/volc-sdk-python/merge_requests/460

--- volcengine/maas/MaasService.py
+++ volcengine/maas/MaasService.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 import json
-import threading
+import copy
 
 from google.protobuf.json_format import Parse
 
@@ -10,8 +10,9 @@ from volcengine.base.Service import Service
 from volcengine.ServiceInfo import ServiceInfo
 from volcengine.auth.SignerV4 import SignerV4
 
+from .ka_mgr import *
 from .sse_decoder import SSEDecoder
-from .models.api.api_pb2 import ChatResp
+from .models.api.api_pb2 import ChatResp, CertResp
 from .exception import MaasException, new_client_sdk_request_error
 
 
@@ -31,17 +32,22 @@ class MaasService(Service):
     def get_api_info():
         api_info = {
             "chat": ApiInfo("POST", "/api/v1/chat", {}, {}, {}),
+            "cert": ApiInfo("POST", "/api/v1/cert", {}, {}, {}),
         }
         return api_info
 
-    def chat(self, req):
+    def chat(self, req, is_secret=False):
         self._validate()
         try:
             req['stream'] = False
+            if is_secret:
+                key, nonce, req = self.encrypt_chat_request(req)
             res = self.json("chat", {}, json.dumps(req))
             if res == '':
                 raise new_client_sdk_request_error("empty response")
             resp = Parse(res, ChatResp(), True)
+            if is_secret:
+                self.decrypt_chat_response(key, nonce, resp)
         except Exception as e:
             try:
                 resp = Parse(str(e.args[0], encoding='utf-8'), ChatResp(), True)
@@ -51,18 +57,20 @@ class MaasService(Service):
                 raise MaasException(resp.error.code_n, resp.error.code, resp.error.message)
         else:
             return resp
-        
+
     def _validate(self):
         if self.service_info.credentials is None or \
             self.service_info.credentials.sk is None or \
             self.service_info.credentials.ak is None:
             raise new_client_sdk_request_error("no valid credential")
 
-    def stream_chat(self, req):
+    def stream_chat(self, req, is_secret=False):
         self._validate()
         
         try:
             req['stream'] = True
+            if is_secret:
+                key, nonce, req = self.encrypt_chat_request(req)
             
             if not ("chat" in self.api_info):
                 raise new_client_sdk_request_error("no such api")
@@ -93,6 +101,8 @@ class MaasService(Service):
                 for data in decoder.next():
                     try:
                         res = Parse(data, ChatResp(), True)
+                        if is_secret:
+                            self.decrypt_chat_response(key, nonce, res)
                     except:
                         pass
                     else:
@@ -105,3 +115,31 @@ class MaasService(Service):
             raise
         except Exception as e:
             raise new_client_sdk_request_error(str(e))
+
+    def init_cert_by_req(self, req):
+        cert_req = {"model": req['model']}
+        try:
+            resp = self.json("cert", {}, json.dumps(cert_req))
+            resp = Parse(resp, CertResp(), True)
+            req['model']['endpoint_id'] = resp.model.endpoint_id
+            return key_agreement_client(resp.cert)
+        except Exception as e:
+            raise new_client_sdk_request_error(str(e))
+
+    def encrypt_chat_request(self, req):
+        req_c = copy.deepcopy(req)
+        
+        cert = self.init_cert_by_req(req_c)
+        key, nonce, token = cert.generate_ecies_key_pair()
+        req_c['crypto_token'] = token
+        for i in req_c['messages']:
+            if i['content'] != '':
+                i['content'] = aes_gcm_encrypt_base64_string(key, nonce, i['content'])
+        
+        return key, nonce, req_c
+
+    def decrypt_chat_response(self, key, nonce, resp):
+        m = resp.choice.message.content
+        if m != '':
+            resp.choice.message.content = aes_gcm_decrypt_base64_string(key, nonce, m)
+        return
---
+++ volcengine/maas/ka_mgr.py
@@ -0,0 +1,106 @@
+import os
+import base64
+import secrets
+from typing import Union
+from tinyec import registry
+from tinyec.ec import Point, Inf
+from cryptography import x509
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives.ciphers import (
+    Cipher, algorithms, modes
+)
+
+
+def aes_gcm_encrypt_bytes(key: bytes, iv: bytes, plain_bytes: bytes, associated_data: bytes = b"") -> bytes:
+    # aes_gcm_encrypt_bytes encrypt message using AES-GCM
+    encryptor = Cipher(
+        algorithms.AES(key),
+        modes.GCM(iv),
+    ).encryptor()
+    # associated_data will be authenticated but not encrypted,
+    # it must also be passed in on decryption.
+    encryptor.authenticate_additional_data(associated_data)
+    # Encrypt the plaintext and get the associated ciphertext.
+    # GCM does not require padding.
+    ciphertext = encryptor.update(plain_bytes) + encryptor.finalize()
+    return ciphertext + encryptor.tag
+
+
+def aes_gcm_encrypt_base64_string(key: bytes, nonce: bytes, plaintext: str) -> str:
+    """aes_gcm_encrypt_base64_string Encrypt message from base64 string to string using AES-GCM
+    """
+    plain_bytes = plaintext.encode()
+    # Encrypt message to string using AES-GCM
+    c = aes_gcm_encrypt_bytes(key, nonce, plain_bytes)
+    return base64.b64encode(c).decode()
+
+
+def aes_gcm_decrypt_bytes(key: bytes, iv: bytes, cipher_bytes: bytes, associated_data: bytes = b"") -> bytes:
+    """aes_gcm_decrypt_bytes Decrypt message from bytes to bytes using AES-GCM
+    """
+    tag_length = 16 # default aes gcm tag length
+    cipher = cipher_bytes[:-tag_length]
+    tag = cipher_bytes[-tag_length:]
+    # Construct a Cipher object, with the key, iv, and additionally the
+    # GCM tag used for authenticating the message.
+    decryptor = Cipher(
+        algorithms.AES(key),
+        modes.GCM(iv, tag),
+    ).decryptor()
+    # We put associated_data back in or the tag will fail to verify
+    # when we finalize the decryptor.
+    decryptor.authenticate_additional_data(associated_data)
+    # Decryption gets us the authenticated plaintext.
+    # If the tag does not match an InvalidTag exception will be raised.
+    return decryptor.update(cipher) + decryptor.finalize()
+
+
+def aes_gcm_decrypt_base64_string(key: bytes, nonce: bytes, ciphertext: str) -> str:
+    # Decrypt message(base64.std.string) using AES-GCM
+    cipher_bytes = base64.decodebytes(ciphertext.encode())
+    return aes_gcm_decrypt_bytes(key, nonce, cipher_bytes).decode()
+
+
+def marshal_tinyec_pub_key(key: Union[Inf, Point]) -> bytes:
+    # python version of crypto/elliptic/elliptic.go Marshal
+    # without point on curve check
+    return bytes([4]) + key.x.to_bytes(32, 'big') + key.y.to_bytes(32, 'big')
+
+
+class key_agreement_client():
+    def __init__(self, pem_path_or_string: str) -> None:
+        """ Load cert and extract public key
+        """
+        if os.path.exists(pem_path_or_string):
+            with open(pem_path_or_string, 'rb') as f:
+                pem_data = f.read()
+        else:
+            pem_data = pem_path_or_string.encode()
+        self._cert = x509.load_pem_x509_certificate(pem_data)
+        cert_pub = self._cert.public_key().public_numbers()
+        self._curve = registry.get_curve(self._cert.public_key().curve.name)
+        self._public_key = Point(self._curve, cert_pub.x, cert_pub.y)
+
+    def generate_ecies_key_pair(self) -> tuple[bytes, bytes, str]:
+        """generate_ecies_key_pair generate ECIES key pair
+        """
+        # Generate an ephemeral elliptic curve scalar and point
+        r = secrets.randbelow(self._curve.field.n)
+        R = r * self._curve.g
+        # Compute shared DH key
+        dh = r * self._public_key
+        # Derive symmetric key and nonce via HKDF
+        marshal_dh = marshal_tinyec_pub_key(dh)
+        length = 32 + 12
+        buf = HKDF(
+            algorithm=hashes.SHA256(),
+            length=length,
+            salt=None,
+            info=None,
+        ).derive(marshal_dh)
+        key = buf[:32]
+        nonce = buf[32:length]
+
+        token = marshal_tinyec_pub_key(R)
+        return key, nonce, base64.b64encode(token).decode()
