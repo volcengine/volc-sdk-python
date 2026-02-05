752e642917e43c2062d1ecd7a8032b53fb3386d6
liyan.90210 <liyan.90210@bytedance.com>
2026-02-05 11:33:14 +0000

    feat auto update sdk

--- volcengine/__init__.py
+++ volcengine/__init__.py
@@ -1,2 +1,2 @@
 # coding:utf-8
-VERSION='v1.0.215'
+VERSION='v1.0.216'
--- volcengine/content_security/ContentSecurityService.py
+++ volcengine/content_security/ContentSecurityService.py
@@ -26,7 +26,7 @@ class ContentSecurityService(Service):
 
     @staticmethod
     def get_service_info():
-        service_info = ServiceInfo("open.volcengineapi.com", {'Accept': 'application/json'},
+        service_info = ServiceInfo("riskcontrol.volcengineapi.com", {'Accept': 'application/json'},
                                    Credentials('', '', 'BusinessSecurity', 'cn-north-1'), 5, 5)
         return service_info
 
