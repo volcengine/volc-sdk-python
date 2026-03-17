volcengine/volc-sdk-python

commit 7b3ec4a7e95338d775bfb43e632a059f5a46eafd

增加了version支持

setup.py
@@ -1,20 +1,20 @@
 # coding:utf-8
 
 from setuptools import setup, find_packages
+from volc import VERSION
 
 setup(
-    name="ttvcloud",
-    version="0.0.31",
-    keywords=("pip", "ttvcloud", "vcloud-sdk-python"),
-    description="The TTvcloud SDK for Python",
+    name="volc",
+    version=VERSION,
+    keywords=("pip", "volc", "volc-sdk-python"),
+    description="The Volcengine SDK for Python",
     license="MIT Licence",
 
-    url="https://github.com/TTvcloud/vcloud-sdk-python",
-    author="TTVcloud SDK",
-    author_email="vcloudplatform@bytedance.com",
+    url="https://github.com/Volcengine/volc-sdk-python",
+    author="Volcengine SDK",    
 
     packages=find_packages(),
     include_package_data=True,
     platforms="any",
-    install_requires=["requests", "pytz", "pycryptodome", "protobuf", "google-api-python-client"]
+    install_requires=["requests", "pytz", "pycryptodome", "protobuf", "google"]
 )

.gitignore
@@ -4,3 +4,4 @@ venv/
 build/
 dist/
 ttvcloud.egg-info/
+volc.egg-info/

ttvcloud/example/__init__.py
@@ -1,1 +1,0 @@
-name = 'ttvcloud'

ttvcloud/example/vod/example_play.py
@@ -1,43 +1,0 @@
-# coding:utf-8
-from __future__ import print_function
-
-from ttvcloud.models.vod.request.request_vod_pb2 import *
-from ttvcloud.vod.VodPlayService import VodPlayService
-
-if __name__ == '__main__':
-    vod_service = VodPlayService()
-    # call below method if you dont set ak and sk in $HOME/.vcloud/config
-    # vod_service.set_ak('ak')
-    # vod_service.set_sk('sk')
-    try:
-        vid = 'v0c2c369007abu04ru8riko30uo9n73g'
-        req = VodGetPlayInfoRequest()
-        req.Vid = vid
-        req.Ssl = '1'
-        resp = vod_service.get_play_info(req)
-    except Exception:
-        raise
-    else:
-        print(resp)
-        if resp.ResponseMetadata.Error.Code == '':
-            print(resp.Result.PlayInfoList[0].MainPlayUrl)
-        else:
-            print(resp.ResponseMetadata.Error)
-    print('*' * 100)
-
-    try:
-        vid = 'v0c2c369007abu04ru8riko30uo9n73g'
-        req2 = VodGetOriginalPlayInfoRequest()
-        req2.Vid = vid
-        req2.Ssl = '1'
-        resp2 = vod_service.get_original_play_info(req2)
-    except Exception:
-        raise
-    else:
-        print(resp2)
-        if resp2.ResponseMetadata.Error.Code == '':
-            print(resp2.Result.MainPlayUrl)
-        else:
-            print(resp2.ResponseMetadata.Error)
-
-    print('*' * 100)

ttvcloud/models/base/base_pb2.py
@@ -1,166 +1,0 @@
-# -*- coding: utf-8 -*-
-# Generated by the protocol buffer compiler.  DO NOT EDIT!
-# source: base/base.proto
-
-from google.protobuf import descriptor as _descriptor
-from google.protobuf import message as _message
-from google.protobuf import reflection as _reflection
-from google.protobuf import symbol_database as _symbol_database
-# @@protoc_insertion_point(imports)
-
-_sym_db = _symbol_database.Default()
-
-
-from google.protobuf import descriptor_pb2 as google_dot_protobuf_dot_descriptor__pb2
-
-
-DESCRIPTOR = _descriptor.FileDescriptor(
-  name='base/base.proto',
-  package='Vcloud.Models.Base',
-  syntax='proto3',
-  serialized_options=b'\n\035com.bytedanceapi.model.commonB\004BaseP\001Z1github.com/TTvcloud/vcloud-sdk-golang/models/base\240\001\001\330\001\001\302\002\000\312\002\022Vcloud\\Models\\Base\342\002\031Vcloud\\Models\\GPBMetadata',
-  create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x0f\x62\x61se/base.proto\x12\x12Vcloud.Models.Base\x1a google/protobuf/descriptor.proto\"\x99\x01\n\x10ResponseMetadata\x12\x11\n\tRequestId\x18\x01 \x01(\t\x12\x0e\n\x06\x41\x63tion\x18\x02 \x01(\t\x12\x0f\n\x07Version\x18\x03 \x01(\t\x12\x0f\n\x07Service\x18\x04 \x01(\t\x12\x0e\n\x06Region\x18\x05 \x01(\t\x12\x30\n\x05\x45rror\x18\x06 \x01(\x0b\x32!.Vcloud.Models.Base.ResponseError\".\n\rResponseError\x12\x0c\n\x04\x43ode\x18\x01 \x01(\t\x12\x0f\n\x07Message\x18\x02 \x01(\t:6\n\x0cproduct_type\x12\x1c.google.protobuf.FileOptions\x18\x90N \x01(\t\x88\x01\x01\x42\x94\x01\n\x1d\x63om.bytedanceapi.model.commonB\x04\x42\x61seP\x01Z1github.com/TTvcloud/vcloud-sdk-golang/models/base\xa0\x01\x01\xd8\x01\x01\xc2\x02\x00\xca\x02\x12Vcloud\\Models\\Base\xe2\x02\x19Vcloud\\Models\\GPBMetadatab\x06proto3'
-  ,
-  dependencies=[google_dot_protobuf_dot_descriptor__pb2.DESCRIPTOR,])
-
-
-PRODUCT_TYPE_FIELD_NUMBER = 10000
-product_type = _descriptor.FieldDescriptor(
-  name='product_type', full_name='Vcloud.Models.Base.product_type', index=0,
-  number=10000, type=9, cpp_type=9, label=1,
-  has_default_value=False, default_value=b"".decode('utf-8'),
-  message_type=None, enum_type=None, containing_type=None,
-  is_extension=True, extension_scope=None,
-  serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key)
-
-
-_RESPONSEMETADATA = _descriptor.Descriptor(
-  name='ResponseMetadata',
-  full_name='Vcloud.Models.Base.ResponseMetadata',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  create_key=_descriptor._internal_create_key,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='RequestId', full_name='Vcloud.Models.Base.ResponseMetadata.RequestId', index=0,
-      number=1, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='Action', full_name='Vcloud.Models.Base.ResponseMetadata.Action', index=1,
-      number=2, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='Version', full_name='Vcloud.Models.Base.ResponseMetadata.Version', index=2,
-      number=3, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='Service', full_name='Vcloud.Models.Base.ResponseMetadata.Service', index=3,
-      number=4, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='Region', full_name='Vcloud.Models.Base.ResponseMetadata.Region', index=4,
-      number=5, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='Error', full_name='Vcloud.Models.Base.ResponseMetadata.Error', index=5,
-      number=6, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=74,
-  serialized_end=227,
-)
-
-
-_RESPONSEERROR = _descriptor.Descriptor(
-  name='ResponseError',
-  full_name='Vcloud.Models.Base.ResponseError',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  create_key=_descriptor._internal_create_key,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='Code', full_name='Vcloud.Models.Base.ResponseError.Code', index=0,
-      number=1, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='Message', full_name='Vcloud.Models.Base.ResponseError.Message', index=1,
-      number=2, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=229,
-  serialized_end=275,
-)
-
-_RESPONSEMETADATA.fields_by_name['Error'].message_type = _RESPONSEERROR
-DESCRIPTOR.message_types_by_name['ResponseMetadata'] = _RESPONSEMETADATA
-DESCRIPTOR.message_types_by_name['ResponseError'] = _RESPONSEERROR
-DESCRIPTOR.extensions_by_name['product_type'] = product_type
-_sym_db.RegisterFileDescriptor(DESCRIPTOR)
-
-ResponseMetadata = _reflection.GeneratedProtocolMessageType('ResponseMetadata', (_message.Message,), {
-  'DESCRIPTOR' : _RESPONSEMETADATA,
-  '__module__' : 'base.base_pb2'
-  # @@protoc_insertion_point(class_scope:Vcloud.Models.Base.ResponseMetadata)
-  })
-_sym_db.RegisterMessage(ResponseMetadata)
-
-ResponseError = _reflection.GeneratedProtocolMessageType('ResponseError', (_message.Message,), {
-  'DESCRIPTOR' : _RESPONSEERROR,
-  '__module__' : 'base.base_pb2'
-  # @@protoc_insertion_point(class_scope:Vcloud.Models.Base.ResponseError)
-  })
-_sym_db.RegisterMessage(ResponseError)
-
-google_dot_protobuf_dot_descriptor__pb2.FileOptions.RegisterExtension(product_type)
-
-DESCRIPTOR._options = None
-# @@protoc_insertion_point(module_scope)

ttvcloud/models/vod/business/vod_play_pb2.py
@@ -1,241 +1,0 @@
-# -*- coding: utf-8 -*-
-# Generated by the protocol buffer compiler.  DO NOT EDIT!
-# source: vod/business/vod_play.proto
-
-from google.protobuf import descriptor as _descriptor
-from google.protobuf import message as _message
-from google.protobuf import reflection as _reflection
-from google.protobuf import symbol_database as _symbol_database
-# @@protoc_insertion_point(imports)
-
-_sym_db = _symbol_database.Default()
-
-
-from ttvcloud.models.vod.business import vod_common_pb2 as vod_dot_business_dot_vod__common__pb2
-
-
-DESCRIPTOR = _descriptor.FileDescriptor(
-  name='vod/business/vod_play.proto',
-  package='Vcloud.Models.Vod',
-  syntax='proto3',
-  serialized_options=b'\n#com.bytedanceapi.model.vod.businessB\007VodPlayP\001Z9github.com/TTvcloud/vcloud-sdk-golang/models/vod/business\240\001\001\330\001\001\302\002\000\312\002\032Vcloud\\Models\\Vod\\Business\342\002\031Vcloud\\Models\\GPBMetadata',
-  create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x1bvod/business/vod_play.proto\x12\x11Vcloud.Models.Vod\x1a\x1dvod/business/vod_common.proto\"\x86\x02\n\x14VodGetPlayInfoResult\x12\x0b\n\x03Vid\x18\x01 \x01(\t\x12\x0e\n\x06Status\x18\x02 \x01(\x03\x12\x11\n\tPosterUrl\x18\x03 \x01(\t\x12\x10\n\x08\x44uration\x18\x04 \x01(\x02\x12\x10\n\x08\x46ileType\x18\x05 \x01(\t\x12\x16\n\x0e\x45nableAdaptive\x18\x06 \x01(\x08\x12\x12\n\nTotalCount\x18\x07 \x01(\x03\x12\x38\n\x0c\x41\x64\x61ptiveInfo\x18\x08 \x01(\x0b\x32\".Vcloud.Models.Vod.VodAdaptiveInfo\x12\x34\n\x0cPlayInfoList\x18\t \x03(\x0b\x32\x1e.Vcloud.Models.Vod.VodPlayInfo\"\xd8\x01\n\x1cVodGetOriginalPlayInfoResult\x12\x10\n\x08\x46ileType\x18\x01 \x01(\t\x12\x10\n\x08\x44uration\x18\x02 \x01(\x02\x12\x0c\n\x04Size\x18\x03 \x01(\x03\x12\x0e\n\x06Height\x18\x04 \x01(\x03\x12\r\n\x05Width\x18\x05 \x01(\x03\x12\x0e\n\x06\x46ormat\x18\x06 \x01(\t\x12\r\n\x05\x43odec\x18\x07 \x01(\t\x12\x0f\n\x07\x42itrate\x18\x08 \x01(\x02\x12\x0b\n\x03Md5\x18\t \x01(\t\x12\x13\n\x0bMainPlayUrl\x18\n \x01(\t\x12\x15\n\rBackupPlayUrl\x18\x0b \x01(\tB\xad\x01\n#com.bytedanceapi.model.vod.businessB\x07VodPlayP\x01Z9github.com/TTvcloud/vcloud-sdk-golang/models/vod/business\xa0\x01\x01\xd8\x01\x01\xc2\x02\x00\xca\x02\x1aVcloud\\Models\\Vod\\Business\xe2\x02\x19Vcloud\\Models\\GPBMetadatab\x06proto3'
-  ,
-  dependencies=[vod_dot_business_dot_vod__common__pb2.DESCRIPTOR,])
-
-
-
-
-_VODGETPLAYINFORESULT = _descriptor.Descriptor(
-  name='VodGetPlayInfoResult',
-  full_name='Vcloud.Models.Vod.VodGetPlayInfoResult',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  create_key=_descriptor._internal_create_key,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='Vid', full_name='Vcloud.Models.Vod.VodGetPlayInfoResult.Vid', index=0,
-      number=1, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='Status', full_name='Vcloud.Models.Vod.VodGetPlayInfoResult.Status', index=1,
-      number=2, type=3, cpp_type=2, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='PosterUrl', full_name='Vcloud.Models.Vod.VodGetPlayInfoResult.PosterUrl', index=2,
-      number=3, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='Duration', full_name='Vcloud.Models.Vod.VodGetPlayInfoResult.Duration', index=3,
-      number=4, type=2, cpp_type=6, label=1,
-      has_default_value=False, default_value=float(0),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='FileType', full_name='Vcloud.Models.Vod.VodGetPlayInfoResult.FileType', index=4,
-      number=5, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='EnableAdaptive', full_name='Vcloud.Models.Vod.VodGetPlayInfoResult.EnableAdaptive', index=5,
-      number=6, type=8, cpp_type=7, label=1,
-      has_default_value=False, default_value=False,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='TotalCount', full_name='Vcloud.Models.Vod.VodGetPlayInfoResult.TotalCount', index=6,
-      number=7, type=3, cpp_type=2, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='AdaptiveInfo', full_name='Vcloud.Models.Vod.VodGetPlayInfoResult.AdaptiveInfo', index=7,
-      number=8, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='PlayInfoList', full_name='Vcloud.Models.Vod.VodGetPlayInfoResult.PlayInfoList', index=8,
-      number=9, type=11, cpp_type=10, label=3,
-      has_default_value=False, default_value=[],
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=82,
-  serialized_end=344,
-)
-
-
-_VODGETORIGINALPLAYINFORESULT = _descriptor.Descriptor(
-  name='VodGetOriginalPlayInfoResult',
-  full_name='Vcloud.Models.Vod.VodGetOriginalPlayInfoResult',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  create_key=_descriptor._internal_create_key,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='FileType', full_name='Vcloud.Models.Vod.VodGetOriginalPlayInfoResult.FileType', index=0,
-      number=1, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='Duration', full_name='Vcloud.Models.Vod.VodGetOriginalPlayInfoResult.Duration', index=1,
-      number=2, type=2, cpp_type=6, label=1,
-      has_default_value=False, default_value=float(0),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='Size', full_name='Vcloud.Models.Vod.VodGetOriginalPlayInfoResult.Size', index=2,
-      number=3, type=3, cpp_type=2, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='Height', full_name='Vcloud.Models.Vod.VodGetOriginalPlayInfoResult.Height', index=3,
-      number=4, type=3, cpp_type=2, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='Width', full_name='Vcloud.Models.Vod.VodGetOriginalPlayInfoResult.Width', index=4,
-      number=5, type=3, cpp_type=2, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='Format', full_name='Vcloud.Models.Vod.VodGetOriginalPlayInfoResult.Format', index=5,
-      number=6, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='Codec', full_name='Vcloud.Models.Vod.VodGetOriginalPlayInfoResult.Codec', index=6,
-      number=7, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='Bitrate', full_name='Vcloud.Models.Vod.VodGetOriginalPlayInfoResult.Bitrate', index=7,
-      number=8, type=2, cpp_type=6, label=1,
-      has_default_value=False, default_value=float(0),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='Md5', full_name='Vcloud.Models.Vod.VodGetOriginalPlayInfoResult.Md5', index=8,
-      number=9, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='MainPlayUrl', full_name='Vcloud.Models.Vod.VodGetOriginalPlayInfoResult.MainPlayUrl', index=9,
-      number=10, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='BackupPlayUrl', full_name='Vcloud.Models.Vod.VodGetOriginalPlayInfoResult.BackupPlayUrl', index=10,
-      number=11, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=347,
-  serialized_end=563,
-)
-
-_VODGETPLAYINFORESULT.fields_by_name['AdaptiveInfo'].message_type = vod_dot_business_dot_vod__common__pb2._VODADAPTIVEINFO
-_VODGETPLAYINFORESULT.fields_by_name['PlayInfoList'].message_type = vod_dot_business_dot_vod__common__pb2._VODPLAYINFO
-DESCRIPTOR.message_types_by_name['VodGetPlayInfoResult'] = _VODGETPLAYINFORESULT
-DESCRIPTOR.message_types_by_name['VodGetOriginalPlayInfoResult'] = _VODGETORIGINALPLAYINFORESULT
-_sym_db.RegisterFileDescriptor(DESCRIPTOR)
-
-VodGetPlayInfoResult = _reflection.GeneratedProtocolMessageType('VodGetPlayInfoResult', (_message.Message,), {
-  'DESCRIPTOR' : _VODGETPLAYINFORESULT,
-  '__module__' : 'vod.business.vod_play_pb2'
-  # @@protoc_insertion_point(class_scope:Vcloud.Models.Vod.VodGetPlayInfoResult)
-  })
-_sym_db.RegisterMessage(VodGetPlayInfoResult)
-
-VodGetOriginalPlayInfoResult = _reflection.GeneratedProtocolMessageType('VodGetOriginalPlayInfoResult', (_message.Message,), {
-  'DESCRIPTOR' : _VODGETORIGINALPLAYINFORESULT,
-  '__module__' : 'vod.business.vod_play_pb2'
-  # @@protoc_insertion_point(class_scope:Vcloud.Models.Vod.VodGetOriginalPlayInfoResult)
-  })
-_sym_db.RegisterMessage(VodGetOriginalPlayInfoResult)
-
-
-DESCRIPTOR._options = None
-# @@protoc_insertion_point(module_scope)

commit 76965ea3359414d46c4bbb6523caa3b22f9976e8

chore(deps): remove unused google>=3.0.0 dependency
The `google>=3.0.0` dependency was introduced accidentally in commit
7b3ec4a7 (2020-11-09) when `google-api-python-client` was shortened
to `google` during a package rebranding from ttvcloud to volc.

The `google` PyPI package (by Mario Vilas) is a Google Search scraper —
completely unrelated to `google.protobuf`. All `from google.protobuf`
imports in this codebase are satisfied by the `protobuf` package, which
is already listed as a separate dependency.

Zero files in this repository import anything from the `google` PyPI
package. Removing it also drops the unnecessary transitive dependency
on `beautifulsoup4`.

setup.py
@@ -10,7 +10,6 @@ install_requires = [
     "pytz>=2020.5",
     "pycryptodome>=3.9.9,<4.0.0",
     "protobuf>=3.18.3",
-    "google>=3.0.0",
     "six>=1.0",
 ]
 